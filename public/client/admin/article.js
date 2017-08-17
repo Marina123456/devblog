4c62643688fb8fdd40be6d707afaea9b6f6d14ef
Marina123456 <marjnazhjrnova@yandex.ru>
2017-08-18 01:20:20 +0400

    12

--- public/client/admin/article.js
+++ public/client/admin/article.js
@@ -3,5 +3,6 @@ buttonFormAdd.addEventListener('click',function(){
 	
 	let strPost=$('#add-article').serialize();
 	let editorData = $('textarea#editor1').val();
+	strPost+=editorData;
 	alert(strPost);
 });
