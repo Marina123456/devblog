4ce7b1ca9998fe770a84c13c42ba06c9d34a0299
Marina123456 <marjnazhjrnova@yandex.ru>
2017-08-18 11:11:59 +0400

    12

--- public/client/admin/article.js
+++ public/client/admin/article.js
@@ -1,19 +1,17 @@
 let buttonFormAdd=document.querySelector('#add-article-but');
 buttonFormAdd.addEventListener('click',function(){
 	
-	let name=document.querySelector('#add-article #name');
-	let category=document.querySelector('#add-article #category');
-	let image=document.querySelector('#add-article #image');
-	let message = CKEDITOR.instances['message'].getData();
-	
+	let article={
+		name: document.querySelector('#add-article #name');
+		category: document.querySelector('#add-article #category');
+		image: document.querySelector('#add-article #image');
+		message: CKEDITOR.instances['message'].getData();
+	}	
 	//console.log(strPost);
 	
 	let formData = new FormData();
-	formData.append( "name",name);
-	formData.append( "category",category);
-	formData.append( "image",image);
-	formData.append( "message",message);
-
+	formData.append( "article",article);
+	
 	fetch("/api/article/", {
     	method: "POST",
     	body: formData
