71543c2b8299fe5943166ea4816f8f7ebbe21b0a
Жирнова Марина Анатольевна <226598@niuitmo.ru>
2017-12-18 16:04:48 +0300

    Update all_articles.js

--- public/client/admin/all_articles.js
+++ public/client/admin/all_articles.js
@@ -6,22 +6,22 @@ function init(){
 
             console.log(item);
             let newTr=document.createElement('tr');
-            let newTd1=document.createElement('tr');
+            let newTd1=document.createElement('td');
             newTr.appendChild(newTd1);
             let newText1=document.createTextNode(item.id);
             newTd1.appendChild(newText1);
             
-            let newTd2=document.createElement('tr');
+            let newTd2=document.createElement('td');
             newTr.appendChild(newTd2);
             let newText2=document.createTextNode(item.title);
             newTd2.appendChild(newText2);
             
-            let newTd3=document.createElement('tr');
+            let newTd3=document.createElement('td');
             newTr.appendChild(newTd3);
             let newText3=document.createTextNode(item.categories);
             newTd3.appendChild(newText3);
             
-            let newTd4=document.createElement('tr');
+            let newTd4=document.createElement('td');
             newTr.appendChild(newTd4);
             let newText4=document.createTextNode(item.content);
             newTd4.appendChild(newText4);
