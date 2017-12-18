e5b8906b2ec3b6625ad32f434b2db974ad7efb48
Жирнова Марина Анатольевна <226598@niuitmo.ru>
2017-12-18 16:02:58 +0300

    Update all_articles.js

--- public/client/admin/all_articles.js
+++ public/client/admin/all_articles.js
@@ -1,14 +1,38 @@
 function init(){
     fetch('/api/posts')
     .then(res => res.json())
-    .then(json => console.log(json));
+    .then(json => {
+        json.forEach(function(item, i, json) {
+
+            console.log(item);
+            let newTr=document.createElement('tr');
+            let newTd1=document.createElement('tr');
+            newTr.appendChild(newTd1);
+            let newText1=document.createTextNode(item.id);
+            newTd1.appendChild(newText1);
+            
+            let newTd2=document.createElement('tr');
+            newTr.appendChild(newTd2);
+            let newText2=document.createTextNode(item.title);
+            newTd2.appendChild(newText2);
+            
+            let newTd3=document.createElement('tr');
+            newTr.appendChild(newTd3);
+            let newText3=document.createTextNode(item.categories);
+            newTd3.appendChild(newText3);
+            
+            let newTd4=document.createElement('tr');
+            newTr.appendChild(newTd4);
+            let newText4=document.createTextNode(item.content);
+            newTd4.appendChild(newText4);
+            
+            
+            document.querySelector('.table tbody').appendChild(newTr);
+    });
+        
+    });
     
-    let newTr=document.createElement('tr');
-    let newTd=document.createElement('tr');
-    newTr.appendChild(newTd);
-    let newText=document.createTextNode('текст, который смог');
-    newTd.appendChild(newText);
-    document.querySelector('.table tbody').appendChild(newTr);
+    
     
 }
 
