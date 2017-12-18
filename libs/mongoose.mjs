4d429a339437283b1d9ffe86831af41b597a5a12
Жирнова Марина Анатольевна <226598@niuitmo.ru>
2017-12-19 01:26:34 +0300

    Update mongoose.mjs

--- libs/mongoose.mjs
+++ libs/mongoose.mjs
@@ -68,7 +68,7 @@ var Project = mongoose.model('Project', Project);
 module.exports.Project = Project;
 
 let Article = mongoose.model('Article', ArticleSchema);
-export default function() {return Article;}
+export {Article};
 
 var User = mongoose.model('User', User);
 module.exports.User = User;
