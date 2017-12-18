4afcad4df3e6f5e3beadd78f874611b53e6ab31b
Жирнова Марина Анатольевна <226598@niuitmo.ru>
2017-12-18 23:10:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ let Project = require('./libs/mongoose').Project;
 let Article = require('./libs/mongoose').Article;
 let User = require('./libs/mongoose').User;
 //----//
-
+app.use('/api/posts', posts.rtr(express));
 app.get('/', function (req, res) {
     ArticleSmallModel.find(function (err, articlesSmall) {
         res.render('index', {articlesSmall: articlesSmall});
@@ -57,62 +57,6 @@ app.get('/article_:id', function (req, res) {
         res.render('article',{article:article});        
     });    
 });
-app.get('/api/posts/:id', function (req, res) {
-    Article.findOne({ 'articleId': req.params["id"]}, function(err, article){
-	    
-    article.message=article.message.replace(new RegExp("\\\n",'g'),"прошел");
-    res.send({
-        id:         article.articleId,
-        title:      article.name,
-        categories: article.category,
-        content:    article.message
-    });    
-    });
-    
-});
-app.get('/api/posts', function (req, res) {
-    Article.find(function(err, article){
-	
-	let posts=article.map(function(elem, index) {	
-        	let lem_new={
-            	id:         elem.articleId,
-            	title:      elem.name,
-            	categories: elem.category,
-            	content:    elem.message.replace(new RegExp("\\\n",'g'),"прошел")
-        	};
-	return lem_new; });
-    	res.send(posts);
-    });
-});
-app.delete('/api/posts/:id', function (req, res) {
-     Article.findOneAndRemove( {'articleId': req.params["id"]}, (err, article) => {  
-   	res.send({
-        	id:         article.articleId,
-        	title:      article.name,
-        	categories: article.category,
-        	content:    article.message
-    });
-   });
-    
-});
-
-app.post('/api/posts/',function(req,res){
-let image = '/blog.png';
-if (req.body.image) image = req.body.image; 	
-    let article = new Article({
-        name: req.body.title,
-        category: req.body.categories,
-	image: image,
-        message: req.body.content
-    });
-    article.save(function (err) {
-        if (!err) {
-            console.log("articleSmall created");
-            return res.send({ status: 'OK', article:article.articleId });
-        } else { console.log(err); }
-    });
-    
-});
 app.post('/api/article/',function(req,res){
     let article = new Article({
         name: req.body.name,
@@ -128,6 +72,7 @@ app.post('/api/article/',function(req,res){
     });
     
 });
+
 app.post('/api/login/',function(req,res){
     let user = req.body.user;
     let password = req.body.password;
