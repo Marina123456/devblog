193ae39dde001772f674a7a9d05c0b6f0645ceab
Жирнова Марина Анатольевна <226598@niuitmo.ru>
2017-12-19 01:47:50 +0300

    Update create-article.mjs

--- controllers/articles/create-article.mjs
+++ controllers/articles/create-article.mjs
@@ -3,17 +3,17 @@ import {Article} from '../../libs/mongoose';
 
 export async r=>{
 let image = '/blog.png';
-if (req.body.image) image = req.body.image; 	
+if (r.req.body.image) image = r.req.body.image; 	
     let article = new Article({
-        name: req.body.title,
-        category: req.body.categories,
+        name: r.req.body.title,
+        category: r.req.body.categories,
 	image: image,
-        message: req.body.content
+        message: r.req.body.content
     });
     article.save(function (err) {
         if (!err) {
             console.log("articleSmall created");
-            return res.send({ status: 'OK', article:article.articleId });
+            return r.res.send({ status: 'OK', article:article.articleId });
         } else { console.log(err); }
     });
 }
