f501ebf026025d19c30b4532a691dbc0c2c7f710
Жирнова Марина Анатольевна <226598@niuitmo.ru>
2017-12-18 15:13:38 +0300

    Update all_articles.js

--- public/client/admin/all_articles.js
+++ public/client/admin/all_articles.js
@@ -1,6 +1,6 @@
-window.onload = function() {
+function init() {
     let newTr=document.createElement('tr');
-	  let newTd=newTr.createElement('tr');
-	  newTd.createTextNode('текст, который смог');
-	  document.querySelector('.table tbody').appendChild(newLi);
+    let newTd=newTr.createElement('tr');
+    newTd.createTextNode('текст, который смог')
+    document.querySelector('.table tbody').appendChild(newLi);
 };
