2eb2b141c089c85021bb84a20a6527da3fcdc791
Жирнова Марина Анатольевна <226598@niuitmo.ru>
2017-12-18 23:46:35 +0300

    Create get-article.js

---
+++ controllers/articles/get-article.js
@@ -0,0 +1,15 @@
+'use strict';
+import {Article} from '../../libs/mongoose;
+
+export default async r=>{
+    Article.findOne({ 'articleId': r.params["id"]}, function(err, article){
+	    
+    article.message=article.message.replace(new RegExp("\\\n",'g'),"прошел");
+    res.send({
+        id:         article.articleId,
+        title:      article.name,
+        categories: article.category,
+        content:    article.message
+    });    
+    });
+}
