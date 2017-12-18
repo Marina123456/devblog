c2c234f6b0665a1ff7cf07511b26eb61fda68ad8
Жирнова Марина Анатольевна <226598@niuitmo.ru>
2017-12-18 23:57:28 +0300

    Create delete-article.js

---
+++ controllers/articles/delete-article.js
@@ -0,0 +1,13 @@
+'use strict';
+import {Article} from '../../libs/mongoose';
+
+export default async r=>{
+    Article.findOneAndRemove( {'articleId': req.params["id"]}, (err, article) => {  
+   	res.send({
+        	id:         article.articleId,
+        	title:      article.name,
+        	categories: article.category,
+        	content:    article.message
+    });
+   });
+}
