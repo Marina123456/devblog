4f499307a8d1744c8be1d4728a2c92eb241a85d3
Жирнова Марина Анатольевна <226598@niuitmo.ru>
2017-12-18 15:40:03 +0300

    Update all_articles.js

--- public/client/admin/all_articles.js
+++ public/client/admin/all_articles.js
@@ -1,8 +1,9 @@
-let pageArticles=document.querySelector('body');
-pageArticles.addEventListener('load', function(){
+function init(){
     let newTr=document.createElement('tr');
     let newTd=newTr.createElement('tr');
     newTd.createTextNode('текст, который смог')
     document.querySelector('.table tbody').appendChild(newTr);
     alert('ok');
-});
+}
+
+init();
