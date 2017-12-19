c12a9bc2f13ebbeb2bac7e3a59b50949e3843c37
Marina123456 <marjnazhjrnova@yandex.ru>
2017-12-19 22:52:01 +0300

    89

--- models/Article.mjs
+++ models/Article.mjs
@@ -1,22 +1,4 @@
-import mongoose from 'mongoose';
-import config from '../db/config';
-import autoIncrement from 'mongoose-auto-increment';
-
-mongoose.connect(config.get('mongoose:uri'));
-
-    
-let db = mongoose.connection;
-db.on('error', function (err) {
-            console.log('connection error:', err.message);
-});
-db.once('open', function callback () {
-    console.log("Connected to DB!");
-});
-
-autoIncrement.initialize(db);
-
-
-let Schema = mongoose.Schema;
+import {Schema, autoIncrement} from './Schema';
 
 let ArticleSchema = new Schema({
     name: { type: String, required: true },
@@ -26,6 +8,7 @@ let ArticleSchema = new Schema({
     modified: { type: Date, default: Date.now },
     message: { type: String, required: true }
 });
+
 ArticleSchema.plugin(autoIncrement.plugin, 'Article');
 ArticleSchema.plugin(autoIncrement.plugin, { 
     model: 'Article', 
---
+++ models/Schema.mjs
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import config from '../db/config';
+import autoIncrement from 'mongoose-auto-increment';
+
+mongoose.connect(config.get('mongoose:uri'));
+
+    
+let db = mongoose.connection;
+db.on('error', function (err) {
+            console.log('connection error:', err.message);
+});
+db.once('open', function callback () {
+    console.log("Connected to DB!");
+});
+
+autoIncrement.initialize(db);
+
+export autoIncrement;
+export let Schema = mongoose.Schema;
