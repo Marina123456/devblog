4b8351e3c43acfaa5affc774ff10ef34c6b84942
Жирнова Марина Анатольевна <226598@niuitmo.ru>
2017-12-19 04:18:04 +0300

    Update index.mjs

--- routes/posts/index.mjs
+++ routes/posts/index.mjs
@@ -1,23 +1,23 @@
 'use strict';
-//import getArticleController from '../../controllers/articles/get-article';
-//import getArticlesController from '../../controllers/articles/get-articles';
-//import createArticleController from '../../controllers/articles/create-article';
-//import deleteArticleController from '../../controllers/articles/delete-article';
+import getArticleController from '../../controllers/articles/get-article';
+import getArticlesController from '../../controllers/articles/get-articles';
+import createArticleController from '../../controllers/articles/create-article';
+import deleteArticleController from '../../controllers/articles/delete-article';
 
-export class Router {
+export default class Router {
    static rtr(x) {
         const r = x.Router();
         r
           .route('/')
-          //.get (getArticlesController)
-          //.post(createArticleController)
+          .get (getArticlesController)
+          .post(createArticleController)
           
         ;
 
         r
           .route('/:id')
-          //.get (getArticleController)
-          //.delete(deleteArticleController)
+          .get (getArticleController)
+          .delete(deleteArticleController)
         ;
         return r;
   }
